47a4e0be8e69c53561fbac3d74201f84e20e5304
YB1010 <37540709+YB1010@users.noreply.github.com>
2018-05-04 15:07:21 +1000

    createSquareBipyramid

---
+++ tutes/assignment2.js
@@ -0,0 +1,168 @@
+/**
+ * cube.js 
+ * A simple Three.js program which draws a cube
+ */
+
+var scene;
+var camera;
+var renderer;
+var upperArm;
+var lowerArm;
+var shoulder;
+var elbow ;
+init();
+var light  = new THREE.DirectionalLight(0xffffff);
+
+light.position.set(0, 1, 0);
+var evlight  = new THREE.AmbientLight(0x404040);
+scene.add(evlight);
+scene.add(light);
+scene.add(createSquareBipyramid(0.5, 0.25, 0.25, 0xaadfff));
+scene.add(createAxes(5));
+
+
+
+renderer.render(scene, camera); 
+var controls = new THREE.TrackballControls(camera);
+controls.addEventListener('change', render);
+animate();
+
+
+
+document.onkeydown = handleKeyDown;
+document.onkeyup = handleKeyUp;
+function handleKeyDown(event)
+{
+    switch (event.keyCode) {
+    case 37:
+        shoulder.rotation.z += 1 * Math.PI / 180;
+        break;
+    case 39:
+        shoulder.rotation.z -= 1 * Math.PI / 180;
+        break;
+	case 38:
+        elbow.rotation.z += 1 * Math.PI / 180;
+        break;
+    case 40:
+        elbow.rotation.z -= 1 * Math.PI / 180;
+        break;
+    }
+}
+	
+
+function init(){
+	scene = new THREE.Scene();
+	camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000); 
+	camera.position.z = 2;
+	//var camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -1, 1);
+	renderer = new THREE.WebGLRenderer();
+	renderer.setSize(window.innerWidth, window.innerHeight);
+	renderer.setClearColor(0x404040, 1);
+	document.body.appendChild(renderer.domElement); 
+}
+//function createCube(){
+//	var geometry = new THREE.Geometry();
+//geometry.vertices.push(new THREE.Vector3(-1, -1, 0));
+//geometry.vertices.push(new THREE.Vector3(1, -1, 0));
+//geometry.vertices.push(new THREE.Vector3(1, 1, 0));
+//geometry.vertices.push(new THREE.Vector3(-1, 1, 0));
+//geometry.faces.push(new THREE.Face3(0, 1, 2));
+//geometry.faces.push(new THREE.Face3(0, 2, 3));
+//geometry.computeFaceNormals();
+//var material = new THREE.MeshBasicMaterial({color: 0xffff00}); 
+//object = new THREE.Mesh(geometry, material); 
+//return object;
+//}
+function createCube(color1){
+	
+	var geometry = new THREE.BoxGeometry(2, 1, 1); 
+	var material = new THREE.MeshBasicMaterial({color:color1}); 
+	var obj = new THREE.Mesh(geometry, material); 
+	obj.add(createAxes(3));
+	return obj;
+}
+function createAxes(length){
+  var geometry = new THREE.Geometry();
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(length, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(0, length, 0));
+  geometry.vertices.push(new THREE.Vector3(0, 0, 0));
+  geometry.vertices.push(new THREE.Vector3(0, 0, length));
+  geometry.colors.push(new THREE.Color(0xff0000));
+  geometry.colors.push(new THREE.Color(0xff0000));
+  geometry.colors.push(new THREE.Color(0x00ff00));
+  geometry.colors.push(new THREE.Color(0x00ff00));
+  geometry.colors.push(new THREE.Color(0x0000ff));
+  geometry.colors.push(new THREE.Color(0x0000ff));
+  var material = new THREE.LineBasicMaterial();
+  material.vertexColors = THREE.VertexColors;
+  var axes = new THREE.LineSegments(geometry, material);
+  axes.name = "axes";
+  return axes;
+}
+function render() {
+    renderer.render(scene, camera); 
+}
+function animate() {
+    render();
+    requestAnimationFrame(animate);
+    controls.update();
+}
+
+  // returns square bipyramid (octahedron) object
+  function createSquareBipyramid(sizeX, sizeY, sizeZ, colors){
+	var geometry = new THREE.Geometry();
+	geometry.vertices.push(new THREE.Vector3(sizeX, 0, 0));
+	geometry.vertices.push(new THREE.Vector3(0, sizeY, 0));
+	geometry.vertices.push(new THREE.Vector3(0, 0, sizeZ));
+	
+	geometry.vertices.push(new THREE.Vector3(-sizeX, 0, 0));
+	geometry.vertices.push(new THREE.Vector3(0, -sizeY, 0));
+	geometry.vertices.push(new THREE.Vector3(0, 0, -sizeZ));
+	
+	geometry.faces.push(new THREE.Face3(0, 1, 2));
+	
+	geometry.faces.push(new THREE.Face3(3, 2, 1));
+	
+	geometry.faces.push(new THREE.Face3(0, 5, 1));
+	
+	geometry.faces.push(new THREE.Face3(3, 1, 5));
+	geometry.faces.push(new THREE.Face3(0, 2, 4));
+	
+	geometry.faces.push(new THREE.Face3(3, 4, 2));
+	geometry.faces.push(new THREE.Face3(0, 4, 5));
+	geometry.faces.push(new THREE.Face3(3, 5, 4));
+	
+	
+	geometry.computeFaceNormals();
+	var material = new THREE.MeshLambertMaterial({color: 0x00ff00});
+	var object = new THREE.Mesh(geometry, material); 
+	object.add(createAxes(3));
+	return object;
+	  
+  }
+
+  // returns pentagonal bipyramid (decahedron) object
+  function createPentagonalBipyramid(sizeX, sizeY, sizeZ, material){}
+
+  // returns joint axes object
+  function createJoint(name){}
+
+  // returns a whole leg
+  function createLeg(end, side, material){}
+
+  // returns the torso object
+  function createTorso(material){}
+
+  // returns the head object
+  function createHead(material){}
+
+  // Uses the other functions to create the frog
+  function createFrog(material){}
+    
+//function animate(){
+//  renderer.render(scene, camera); 
+//  object.rotation.z += 2 * Math.PI / 360;
+//  requestAnimationFrame(animate)
+//}
