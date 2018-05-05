f236ac645e4a5a82efff9cade40c622b3ef0c685
YB1010 <37540709+YB1010@users.noreply.github.com>
2018-05-05 23:37:41 +1000

    hips

--- tutes/assignment2.js
+++ tutes/assignment2.js
@@ -6,13 +6,15 @@ var lowerArm;
 var shoulder;
 var elbow ;
 init();
+
 var light  = new THREE.DirectionalLight(0xffffff);
 
 light.position.set(0, 1, 0);
-var evlight  = new THREE.AmbientLight(0x404040);
+var evlight  = new THREE.AmbientLight(0xffffff);
 scene.add(evlight);
 scene.add(light);
-scene.add(createPentagonalBipyramid(0x00ff00));
+scene.add(createLeg(1, 0x404040,true));
+scene.add(createLeg(1, 0x404040,false));
 scene.add(createAxes(5));
 
 
@@ -85,7 +87,7 @@ function animate() {
 }
 
   // returns square bipyramid (octahedron) object
-  function createSquareBipyramid(size, colors){
+  function createSquareBipyramid(colors, isToes){
 	var geometry = new THREE.Geometry();
 	geometry.vertices.push(new THREE.Vector3(0.5, 0, 0));
 	geometry.vertices.push(new THREE.Vector3(0, 0.25, 0));
@@ -95,22 +97,21 @@ function animate() {
 	geometry.vertices.push(new THREE.Vector3(0, -0.25, 0));
 	geometry.vertices.push(new THREE.Vector3(0, 0, -0.25));
 	
-	geometry.faces.push(new THREE.Face3(0, 1, 2));
 	
-	geometry.faces.push(new THREE.Face3(3, 2, 1));
-	
-	geometry.faces.push(new THREE.Face3(0, 5, 1));
-	
-	geometry.faces.push(new THREE.Face3(3, 1, 5));
-	geometry.faces.push(new THREE.Face3(0, 2, 4));
-	
-	geometry.faces.push(new THREE.Face3(3, 4, 2));
-	geometry.faces.push(new THREE.Face3(0, 4, 5));
-	geometry.faces.push(new THREE.Face3(3, 5, 4));
+		geometry.faces.push(new THREE.Face3(0, 1, 2));
+		geometry.faces.push(new THREE.Face3(3, 2, 1));
+		geometry.faces.push(new THREE.Face3(0, 5, 1));
+		geometry.faces.push(new THREE.Face3(3, 1, 5));
+		geometry.faces.push(new THREE.Face3(0, 2, 4));
+		geometry.faces.push(new THREE.Face3(3, 4, 2));
+		geometry.faces.push(new THREE.Face3(0, 4, 5));
+		geometry.faces.push(new THREE.Face3(3, 5, 4));
 	
 	
 	geometry.computeFaceNormals();
 	var material = new THREE.MeshLambertMaterial({color: colors});
+	
+	
 	var object = new THREE.Mesh(geometry, material); 
 	object.add(createAxes(3));
 	return object;
@@ -145,10 +146,100 @@ function animate() {
   }
 
   // returns joint axes object
-  function createJoint(name){}
+  function createJoint(name){
+	  name=new THREE.Object3D;
+	  return name;
+  }
+  
+   function createToes(colors){
+	   
+		var geometry = new THREE.Geometry();
+		geometry.vertices.push(new THREE.Vector3(0,0,0));
+		geometry.vertices.push(new THREE.Vector3(0.5,0,-0.25));
+		geometry.vertices.push(new THREE.Vector3(0.5,0,0.25));
+		
+		geometry.faces.push(new THREE.Face3(0,1,2));
+		geometry.computeFaceNormals();
+		var material = new THREE.MeshLambertMaterial({color: colors});
+	
+	
+		var object = new THREE.Mesh(geometry, material); 
+		object.add(createAxes(3));
+		
+		return object;
+  }
+
 
   // returns a whole leg
-  function createLeg(end, side, material){}
+  function createLeg(side, material, isleft)
+  {
+	  var rightAdjust;
+	  if(isleft){
+		  rightAdjust=1;
+	  }
+	  else
+	  {
+		  rightAdjust=-1;
+	  }
+	var hip;
+	hip=createJoint(hip);
+	var knee;
+	knee=createJoint(knee);
+	var ankle;
+	ankle=createJoint(ankle);
+	var tmt;
+	tmt=createJoint(tmt);
+	
+	
+	var upLeg;
+	upLeg=createSquareBipyramid(0x00ff00);
+	var lowLeg;
+	lowLeg=createSquareBipyramid(0x00ff00);
+	var foot;
+	foot=createSquareBipyramid(0x00ff00);
+	var toes;
+	toes=createToes(0x00ff00);
+	toes.material.side = THREE.DoubleSide;
+	
+	hip.add(upLeg);
+	
+	hip.add(knee);
+	hip.add(lowLeg);
+	hip.add(ankle);
+	hip.add(foot);
+	hip.add(tmt);
+	hip.add(toes);
+	
+	knee.add(lowLeg);
+	knee.add(ankle);
+	knee.add(foot);
+	knee.add(tmt);
+	knee.add(toes);
+	
+	ankle.add(foot);
+	ankle.add(tmt);
+	ankle.add(toes);
+	
+	tmt.add(toes);
+	
+	hip.rotation.y= rightAdjust*1.575;
+	upLeg.position.x=0.5;
+	
+	knee.position.x=1;
+	knee.rotation.z=-2.214;
+	lowLeg.position.x=0.5;
+	
+	ankle.position.x=1;
+	ankle.rotation.z=2.214;
+	foot.position.x=0.5;
+	
+	tmt.position.x=1;
+	tmt.rotation.y=rightAdjust*-1.575;
+	toes.position.x=0;
+	
+	return hip;
+	
+  }
 
   // returns the torso object
   function createTorso(material){}
